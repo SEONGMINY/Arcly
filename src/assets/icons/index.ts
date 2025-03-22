b97caa133073250b518c81b83d92356ace506831
SEONGMINY <leesm2075@naver.com>
2025-03-22 23:11:37 +0900

    etc: 아이콘 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,1 +1,6 @@
 export { default as Dots } from '@assets/icons/dots.svg';
+export { default as Heart } from '@assets/icons/heart.svg';
+export { default as HeartActive } from '@assets/icons/heart-active.svg';
+export { default as Comment } from '@assets/icons/comment.svg';
+export { default as Repost } from '@assets/icons/repost.svg';
+export { default as Share } from '@assets/icons/share.svg';
