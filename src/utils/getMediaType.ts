5f09f14e908e3cacac6e8468d43bd04c496b0234
SEONGMINY <leesm2075@naver.com>
2025-04-13 16:57:39 +0900

    refactor: getMediaType Media 컴포넌트와 응집도 올리는 리펙토링

--- src/utils/getMediaType.ts
+++ src/utils/getMediaType.ts
@@ -1,22 +1,35 @@
-const IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'svg'];
-const VIDEO_EXTENSIONS = ['mp3', 'mp4', 'mov', 'avi', 'wmv', 'mkv', 'flv'];
+export enum MediaType {
+  IMAGE = 'image',
+  VIDEO = 'video',
+  UNKNOWN = 'unknown',
+}
+
+const MEDIA_EXTENSIONS: Record<MediaType, string[]> = {
+  [MediaType.IMAGE]: ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'svg'],
+  [MediaType.VIDEO]: ['mp3', 'mp4', 'mov', 'avi', 'wmv', 'mkv', 'flv'],
+  [MediaType.UNKNOWN]: [],
+};
 
 const getUrlExtension = (uri: string): string => {
   return uri.split('.').pop()?.toLowerCase() || '';
 };
 
-const getMediaType = (uri: string): 'image' | 'video' | 'unknown' => {
+const getMediaType = (uri: string): MediaType => {
+  if (!uri) {
+    return MediaType.UNKNOWN;
+  }
+
   const extension = getUrlExtension(uri);
 
-  if (IMAGE_EXTENSIONS.includes(extension)) {
-    return 'image';
+  if (MEDIA_EXTENSIONS[MediaType.IMAGE].includes(extension)) {
+    return MediaType.IMAGE;
   }
 
-  if (VIDEO_EXTENSIONS.includes(extension)) {
-    return 'video';
+  if (MEDIA_EXTENSIONS[MediaType.VIDEO].includes(extension)) {
+    return MediaType.VIDEO;
   }
 
-  return 'unknown';
+  return MediaType.UNKNOWN;
 };
 
 export default getMediaType;
